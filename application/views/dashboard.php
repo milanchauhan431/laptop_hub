996b71b86941a978fa7662b73604e48eb2b159a3
Milan Chauhan <milanchauhan431@gmail.com>
2023-09-30 12:15:55 +0530

    set welcome message  in dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,8 +4,20 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/extra-libs/c3/c3.min.css"> -->
 
 <div class="page-wrapper">
-    <div class="container-fluid">
-        
+    <div class="container-fluid bg-container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h4 class="card-title">&#128578; Welcome to Your Dashboard! Have a nice day! &#128578;</h4>
+                            </div>                             
+                        </div>                                         
+                    </div>
+                </div>
+            </div>
+        </div>   
     </div>
 </div>
 
